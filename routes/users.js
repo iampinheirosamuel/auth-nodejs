9aaa23daeb06fc0a321faf7cb9dd697cc966b9be
iampinheirosamuel <pinheirolaoluwa@gmail.com>
2018-10-15 11:25:36 +0100

    feat(views): layout views

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
+});
+
+/* GET register users. */
+router.get('/register', (req, res, next) => {
+  res.render('register', {
+    title: 'Register',
+  });
+});
+
+/* GET member views. */
+router.get('/member', (req, res, next) => {
+  res.render('member', {
+    title: 'Member Area',
+  });
+});
+
+/* GET login views. */
+router.get('/login', (req, res, next) => {
+  res.render('login', {
+    title: 'Login',
+  });
 });
 
 module.exports = router;
